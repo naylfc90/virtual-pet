5157368d3049f2585227ed809ab16ba4247f948d
Nathan Horne <n.horne90@hotmail.co.uk>
2021-10-18 18:54:08 +0100

    added growUp function to Pet

--- test/pet.test.js
+++ test/pet.test.js
@@ -8,4 +8,13 @@ describe('constructor', () => {
       const pet = new Pet('Fido');
       expect(pet.name).toEqual('Fido');
     });
+    it('checks initial age', () => {
+      const pet = new Pet('Fido');
+      expect(pet.age).toEqual(0);
+    });
+    it('checks age after pet has grown up', () => {
+      const pet = new Pet('Fido');
+      pet.growUp();
+      expect(pet.age).toEqual(1);
+    });
   });
